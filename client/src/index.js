5168c6a77864a030bf79c28eee09941793a86049
thongmgnguyen@qasymphony.com <thongmgnguyen@qasymphony.com>
2017-09-19 16:27:23 +0700

    remove theme

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import $ from 'jquery';
 window.jQuery = $;
