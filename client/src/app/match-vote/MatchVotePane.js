c5fadc383fe563993101e65725e35fcb486475e0
thongmgnguyen@qasymphony.com <thongmgnguyen@qasymphony.com>
2017-09-20 11:38:09 +0700

    submit board

--- client/src/app/match-vote/MatchVotePane.js
+++ client/src/app/match-vote/MatchVotePane.js
@@ -4,6 +4,10 @@ import VoteBoard from './VoteBoard.js'
 
 class MatchVotePane extends Component {
 
+  constructor(props) {
+    super(props);
+    this.vote = this.vote.bind(this);
+  }
 
   componentWillMount() {
     let state = {
@@ -16,23 +20,44 @@ class MatchVotePane extends Component {
         id: 2,
         name: "Juventus",
         logo: "https://pre09.deviantart.net/b58c/th/pre/i/2011/268/f/5/juventus_fc_psd_by_chicot101-d4aweqp.png"
-      }
+      },
+      vote: null
     }
 
     this.setState(state);
   }
 
+  vote = (value) => {
+    this.setState({
+      vote: value
+    })
+  }
+
   render() {
     let home = this.state.home;
     let away = this.state.away;
+    let vote = this.state.vote;
+    let submitBoard = null;
+    if (vote != null) {
+      let result;
+      if (vote == 0) {
+        result = "Draw";
+      } else if (vote == home.id) {
+        result = home.name;
+      } else if (vote == away.id) {
+        result = away.name;
+      }
+      submitBoard = <p> {result} </p>;
+    }
     return (
       <div className="panel panel-primary">
         <div className="panel-heading">
           <h3 className="panel-title">Dự Đoán kết quả</h3>
         </div>
         <div className="panel-body">
-          <VoteBoard home={home} away={away} />
+          <VoteBoard home={home} away={away} onVote={this.vote} />
         </div>
+          {submitBoard}
       </div>
     );
   }
--- client/src/app/match-vote/VoteBoard.js
+++ client/src/app/match-vote/VoteBoard.js
@@ -9,33 +9,39 @@ class VoteBoard extends Component {
     onVote: PropTypes.func
   }
 
+  vote= (value) => {
+    if (this.props.onVote) {
+      this.props.onVote(value);
+    }
+  }
+
   render() {
     let home = this.props.home;
     let away = this.props.away;
     return (
-      <form className="btn-group row-eq-height" data-toggle="buttons" role="group">
-        <label style={{width: '40%'}} className="btn btn-success btn-lg">
+      <div className="btn-group row-eq-height" data-toggle="buttons" role="group">
+        <label style={{width: '40%'}} className="btn btn-success btn-lg" onClick={this.vote.bind(this, home.id)}>
         <div className="col-xs-6 col-md-3" style={{height: '100%'}}>
           <a href="#" className="thumbnail">
             <img className="img-responsive" src={home.logo} alt="logo" />
           </a>
         </div>
-          <input type="radio" name="vote" value="w"/><p className="">{home.name} </p>
+          <input type="radio" name="vote" value={home.id} /><p>{home.name} </p>
         </label>
 
-        <label style={{width: '20%', height: '100%'}} className="btn btn-warning btn-lg ">
-          <input type="radio" name="vote" value="d"/>Draw
+        <label style={{width: '20%', height: '100%'}} className="btn btn-warning btn-lg" onClick={this.vote.bind(this, 0)}>
+          <input type="radio" name="vote" value="0" />Draw
         </label>
 
-        <label style={{width: '40%'}} className="btn btn-success btn-lg">
+        <label style={{width: '40%'}} className="btn btn-success btn-lg" onClick={this.vote.bind(this, away.id)}>
           <div className="col-xs-6 col-md-3">
             <a href="#" className="thumbnail">
               <img className="img-responsive" src={away.logo} alt="logo" />
             </a>
           </div>
-          <input type="radio" name="vote" value="l"/><p className="">{away.name} </p>
+          <input type="radio" name="vote" value={away.id} /><p>{away.name} </p>
         </label>
-      </form>
+      </div>
     );
   }
 }
