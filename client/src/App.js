fd00954e8732704608b0fd45d839e7153801f5d8
thongmgnguyen@qasymphony.com <thongmgnguyen@qasymphony.com>
2017-09-25 14:07:33 +0700

    facebook login btn

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
       <div>
         <Header />
         <div className="container-fluid"> 
+        <a href="/auth/facebook" className="btn btn-primary"><span className="fa fa-facebook"></span> Facebook</a>
         <MainPane />
         </div>
       </div>
