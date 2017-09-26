b95e3ea0ac3a9e3511290b2c87b6c4147a766c94
Thong Nguyen <gtzenky@gmail.com>
2017-09-26 21:16:15 +0700

    config prod

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
   'facebookAuth' : {
       'clientID'      : '1525174080892930', // your App ID
       'clientSecret'  : '50f9344f99cf5f0126c72f43d8d6c3f9', // your App Secret
-      'callbackURL'   : 'http://localhost:3001/auth/facebook/callback'
+      'callbackURL'   : 'https://gentle-oasis-80583.herokuapp.com/auth/facebook/callback'
   }
 
   // 'twitterAuth' : {
