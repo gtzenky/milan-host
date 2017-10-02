a1715254d2721f28cdd984ad89a5f940194c28c5
Thong Nguyen <gtzenky@gmail.com>
2017-10-02 09:30:34 +0700

    fix url

--- server.js
+++ server.js
@@ -49,6 +49,10 @@ app.use('/api', leagueRouter);
 // static files ===============================================================
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 // launch ======================================================================
 const port = process.env.PORT || 3000;
 app.listen(port);
